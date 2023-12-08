a8ad147e0c0bb280b7a45cf16ffe01a0d35c9972
Alexander Hergert <alexander.hergert1989@gmail.com>
2023-12-08 12:26:06 +0100

    Create Write Message component for companies.

--- frontend/src/components/ViewApplications.jsx
+++ frontend/src/components/ViewApplications.jsx
@@ -5,6 +5,7 @@ import { TfiWrite } from "react-icons/tfi";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCompanyApps } from "../slices/jobAppsSlice";
+import WriteMessage from "./WriteMessage";
 
 const Style = styled.section`
   position: absolute;
@@ -47,12 +48,13 @@ const ViewApplications = ({ setViewApplications, selectedJob }) => {
   const dispatch = useDispatch();
   const companyApps = useSelector((state) => state.jobApps.jobApps);
   const [enlargedApp, setEnlargedApp] = useState(0);
+  const [isMessageOpen, setIsMessageOpen] = useState(false);
 
   const handleOpen = (i) => {
     setEnlargedApp(i);
   };
 
-  const handleAccept = async (i, status) => {
+  const handleStatus = async (i, status) => {
     const data = {
       job_id: companyJobs[selectedJob]?.job_id,
       user_id: companyApps[i]?.users[i]?.user_id,
@@ -75,6 +77,10 @@ const ViewApplications = ({ setViewApplications, selectedJob }) => {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleWriteMessage = async (i) => {
+    setIsMessageOpen(true);
   };
 
   useEffect(() => {
@@ -102,98 +108,105 @@ const ViewApplications = ({ setViewApplications, selectedJob }) => {
   }, []);
 
   return (
-    <Style>
-      <button
-        className="btn block m-auto border-0 text-white capitalize my-4 bg-red-500 hover:bg-red-200"
-        onClick={() => setViewApplications(false)}
-        aria-label="close"
-      >
-        CLOSE
-      </button>
-      <div
-        className="grid grid-cols-4 justify-center justify-items-center items-center m-auto bg-neutral 
+    <>
+      {isMessageOpen && <WriteMessage setIsMessageOpen={setIsMessageOpen} />}
+      <Style>
+        <button
+          className="btn block m-auto border-0 text-white capitalize my-4 bg-red-500 hover:bg-red-200"
+          onClick={() => setViewApplications(false)}
+          aria-label="close"
+        >
+          CLOSE
+        </button>
+        <div
+          className="grid grid-cols-4 justify-center justify-items-center items-center m-auto bg-neutral 
         max-md:w-[327px] max-md:p-6 md:w-[690px] xl:w-[1100px] shadow rounded-xl text-primary p-4 border-b-4 border-accent"
-      >
-        <div className="col-span-4">
-          <h2 className="pl-12 text-2xl font-bold">Applications</h2>
-        </div>
-        {companyApps[0]?.users[0]?.fullname && (
-          <ul className="col-span-4 mt-6 px-4">
-            {companyApps?.map((app, i) => (
-              <li
-                className="col-span-4 grid xl:grid-cols-4 gap-4 md:grid-cols-2 md:grid-rows-2 xl:grid-rows-1"
-                key={i}
-              >
-                <div className="flex flex-col items-center gap-2">
-                  <img
-                    className="w-[10rem] h-[10rem] max-md:self-start"
-                    src={app.users[i]?.picture}
-                    alt={app.users[i]?.fullname}
-                  />
-                  <h3 className=" max-md:self-start">
-                    {app.users[i]?.fullname}
-                  </h3>
-                  <h3
-                    className={
-                      app.apps[i]?.app_status === "Accepted"
-                        ? "text-green-500"
-                        : "text-red-500"
-                    }
-                  >
-                    {app.apps[i]?.app_status}
-                  </h3>
-                </div>
-                <div className="flex flex-col gap-2">
-                  <h4>{app.users[i]?.email}</h4>
-                  <h4>{app.users[i]?.location}</h4>
-                  <h4>{app.users[i]?.address}</h4>
-                  <h4>{app.users[i]?.skills}</h4>
-                  <h4>
-                    <a
-                      className="text-blue-500 hover:text-blue-800"
-                      href={app.users[i]?.user_website}
-                      target="_blank"
-                    >
-                      {app.users[i]?.user_website}
-                    </a>
-                  </h4>
-                </div>
-                <div
-                  onClick={() => handleOpen(i)}
-                  className={`max-md:px-0 max-xl:px-4 flex flex-col items-center gap-2 ${
-                    enlargedApp === i ? "" : "max-h-[5rem]"
-                  }`}
+        >
+          <div className="col-span-4">
+            <h2 className="pl-12 text-2xl font-bold">Applications</h2>
+          </div>
+          {companyApps[0]?.users[0]?.fullname && (
+            <ul className="col-span-4 mt-6 px-4">
+              {companyApps?.map((app, i) => (
+                <li
+                  className="col-span-4 grid xl:grid-cols-4 gap-4 md:grid-cols-2 md:grid-rows-2 xl:grid-rows-1"
+                  key={i}
                 >
-                  {app.apps[i]?.content}
-                </div>
-                <div className="flex gap-4">
-                  <button className="btn border-0 duration-0 capitalize text-white bg-accent hover:bg-info min-w-[4rem]">
-                    <TfiWrite />
-                  </button>
-                  <button
-                    onClick={() => handleAccept(i, "Accepted")}
-                    className="btn border-0 duration-0 capitalize text-white bg-accent hover:bg-info min-w-[4rem]"
+                  <div className="flex flex-col items-center gap-2">
+                    <img
+                      className="w-[10rem] h-[10rem] max-md:self-start"
+                      src={app.users[i]?.picture}
+                      alt={app.users[i]?.fullname}
+                    />
+                    <h3 className=" max-md:self-start">
+                      {app.users[i]?.fullname}
+                    </h3>
+                    <h3
+                      className={
+                        app.apps[i]?.app_status === "Accepted"
+                          ? "text-green-500"
+                          : "text-red-500"
+                      }
+                    >
+                      {app.apps[i]?.app_status}
+                    </h3>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <h4>{app.users[i]?.email}</h4>
+                    <h4>{app.users[i]?.location}</h4>
+                    <h4>{app.users[i]?.address}</h4>
+                    <h4>{app.users[i]?.skills}</h4>
+                    <h4>
+                      <a
+                        className="text-blue-500 hover:text-blue-800"
+                        href={app.users[i]?.user_website}
+                        target="_blank"
+                      >
+                        {app.users[i]?.user_website}
+                      </a>
+                    </h4>
+                  </div>
+                  <div
+                    onClick={() => handleOpen(i)}
+                    className={`max-md:px-0 max-xl:px-4 flex flex-col items-center gap-2 ${
+                      enlargedApp === i ? "" : "max-h-[5rem]"
+                    }`}
                   >
-                    ACCEPT
-                  </button>
-                  <button
-                    onClick={() => handleAccept(i, "Denied")}
-                    className="btn border-0 duration-0 capitalize text-white bg-red-500 hover:bg-red-200 min-w-[4rem]"
-                  >
-                    DENY
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-        {!companyApps[0]?.users[0]?.fullname && (
-          <h3 className="col-span-4 text-center my-4">
-            You have no Applications for this job.
-          </h3>
-        )}
-      </div>
-    </Style>
+                    {app.apps[i]?.content}
+                  </div>
+                  <div className="flex gap-4">
+                    <button
+                      onClick={() => handleWriteMessage(i)}
+                      aria-label="write message"
+                      className="btn border-0 duration-0 capitalize text-white bg-accent hover:bg-info min-w-[4rem]"
+                    >
+                      <TfiWrite />
+                    </button>
+                    <button
+                      onClick={() => handleStatus(i, "Accepted")}
+                      className="btn border-0 duration-0 capitalize text-white bg-accent hover:bg-info min-w-[4rem]"
+                    >
+                      ACCEPT
+                    </button>
+                    <button
+                      onClick={() => handleStatus(i, "Denied")}
+                      className="btn border-0 duration-0 capitalize text-white bg-red-500 hover:bg-red-200 min-w-[4rem]"
+                    >
+                      DENY
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+          {!companyApps[0]?.users[0]?.fullname && (
+            <h3 className="col-span-4 text-center my-4">
+              You have no Applications for this job.
+            </h3>
+          )}
+        </div>
+      </Style>
+    </>
   );
 };
 
---
+++ frontend/src/components/WriteMessage.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
+
+const Style = styled.section`
+  position: absolute;
+  top: 0;
+  padding-top: 5rem;
+  background-color: rgba(0, 0, 0, 0.75);
+  z-index: 100;
+  width: 100%;
+  min-height: 100dvh;
+
+  input,
+  textarea {
+    width: 100%;
+    border-radius: 5px;
+  }
+
+  textarea {
+    height: 30vh;
+    min-height: 20vh;
+    max-height: 40vh;
+    padding: 1rem;
+    border: 1px solid #ccc;
+  }
+
+  @media screen and (max-width: 767px) {
+    padding-top: 2rem;
+  }
+`;
+
+const WriteMessage = ({ setIsMessageOpen }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    // console.log(data);
+    // try {
+    //   data.job_id = param.jobId;
+    //   const token = await getAccessTokenSilently();
+    //   const response = await axios.post("http://localhost:3000/apply", data, {
+    //     headers: {
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   });
+    //   console.log(response.data);
+    //   dispatch(getApps({ payload: response.data }));
+    //   navigate(`/dashboard`);
+    // } catch (error) {
+    //   console.error("Error calling API:", error);
+    //   console.log(error.response.data);
+    // }
+  };
+
+  return (
+    <Style>
+      <button
+        className="btn block m-auto border-0 text-white capitalize my-4 bg-red-500 hover:bg-red-200"
+        onClick={() => setIsMessageOpen(false)}
+        aria-label="close"
+      >
+        CLOSE
+      </button>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        action=""
+        className="flex flex-col bg-neutral m-auto p-10 max-md:w-[327px] 
+        max-md:p-6 md:w-[690px] xl:w-[1100px] shadow rounded-xl"
+      >
+        <h2 className="text-center font-bold">Write Message</h2>
+        <div className="flex flex-col gap-4 justify-center items-center my-4">
+          <label htmlFor="content">Message Text</label>
+          <textarea
+            className="bg-neutral text-primary"
+            type="text"
+            id="content"
+            {...register("content", {
+              required: true,
+              minLength: 50,
+              maxLength: 500,
+            })}
+            placeholder="Write your message here (min 50 and max 500 characters)"
+            aria-invalid={errors.content ? "true" : "false"}
+          />
+          {errors.content?.type === "required" && (
+            <p className="text-red-500" role="alert">
+              Text is required
+            </p>
+          )}
+          {errors.content?.type === "minLength" && (
+            <p className="text-red-500" role="alert">
+              Please add least 50 characters
+            </p>
+          )}
+          {errors.content?.type === "maxLength" && (
+            <p className="text-red-500" role="alert">
+              You can't type more than 500 characters
+            </p>
+          )}
+        </div>
+        <input
+          type="submit"
+          value="submit message"
+          className="btn my-4 duration-0 capitalize text-white bg-accent max-md:max-w-full 
+          max-w-[10rem] self-center hover:bg-info border-none"
+        />
+      </form>
+    </Style>
+  );
+};
+
+export default WriteMessage;
