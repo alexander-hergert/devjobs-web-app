f3df571c6b5d637d3b2858d34275b1a8b86490cd
Alexander Hergert <alexander.hergert1989@gmail.com>
2026-04-09 13:03:45 +0200

    Update configDB.js for render.com

--- backend/config/configDB.js
+++ backend/config/configDB.js
@@ -1,15 +1,24 @@
 import pkg from "pg";
-import dotenv from "dotenv";
-dotenv.config({ path: "../../.env" });
+import "dotenv/config";
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME || "webdevjobs",
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const isProduction = process.env.ENVIRONMENT === "production";
+
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: isProduction ? { rejectUnauthorized: false } : false,
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME || "webdevjobs",
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT) || 5432,
+        ssl: isProduction ? { rejectUnauthorized: false } : false,
+      },
+);
 
 export default pool;
